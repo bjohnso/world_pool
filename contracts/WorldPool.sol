ecc716b1358afd90cc29b5ead220ebec4df1ecf8
Brandon Johnson <brandon.johnson@Brandon-Johnson.local>
2022-05-04 21:34:57 +0200

    Deposit Escrows

--- contracts/WorldPool.sol
+++ contracts/WorldPool.sol
@@ -9,10 +9,11 @@ contract WorldPool is ReentrancyGuard {
     uint _nonce;
 
     mapping(bytes32 => Pool) public pools;
+    mapping(bytes32 => PoolDepositEscrow) public poolDepositEscrows;
 
     constructor() { _nonce = 0; }
 
-    function createPool(string memory name, string memory description) public {
+    function createPool(string memory name, string memory description, uint256 minStake) public {
         bytes32 poolId = keccak256(abi.encodePacked(block.number, msg.data, _nonce++));
         address owner = msg.sender;
 
@@ -31,13 +32,14 @@ contract WorldPool is ReentrancyGuard {
             owner: owner,
             name: name,
             description: description,
-            balance: 0
+            balance: 0,
+            minStake: minStake
         });
 
         pools[poolId] = pool;
     }
 
-    function updatePool(bytes32 poolId, string memory name, string memory description) public {
+    function updatePool(bytes32 poolId, string memory name, string memory description, uint256 minStake) public {
         address owner = msg.sender;
 
         require(
@@ -64,6 +66,79 @@ contract WorldPool is ReentrancyGuard {
         if (!compareStrings(pool.description, description)) {
             pool.description = description;
         }
+
+        if (minStake != pool.minStake) {
+            pool.minStake = minStake;
+        }
+
+        pools[poolId] = pool;
+    }
+
+    function deletePool(bytes32 poolId) {
+        address owner = msg.sender;
+
+        require(
+            pools[poolId].owner != address(0x0),
+            "No Pool exists for this ID."
+        );
+
+        require(
+            pools[poolId].owner == owner,
+            "A transaction signer must own the Pool they wish to delete."
+        );
+
+        // TODO : Payout contributors
+        // TODO : Payout admin
+
+        delete pools[poolId];
+    }
+
+    function createUserEscrow(bytes32 poolId) public payable {
+        bytes32 escrowId = keccak256(abi.encodePacked(block.number, msg.data, _nonce++));
+
+        require(
+            pools[poolId].owner != address(0x0),
+            "No Pool exists for this ID."
+        );
+
+        require(
+            poolDepositEscrows[escrowId].owner == address(0x0),
+            "Could not generate unique Pool Deposit Escrow ID."
+        );
+
+        require(
+            pools[poolId].minStake <= msg.value,
+            "Deposit does not meet the minimum requirement."
+        );
+
+        PoolDepositEscrow memory poolDepositEscrow = PoolDepositEscrow({
+            id: escrowId,
+            owner: owner,
+            poolId: poolId,
+            balance: 0
+        });
+
+        poolDepositEscrow.balance += msg.value;
+        poolDepositEscrows[escrowId] = poolDepositEscrow;
+    }
+
+    function depositIntoUserEscrow(bytes escrowId) public payable {
+        PoolDepositEscrow memory poolDepositEscrow = poolDepositEscrows[escrowId];
+
+        require(
+            poolDepositEscrow.owner != address(0x0),
+            "No Pool Deposit Escrow exists for this ID."
+        );
+
+        bytes32 poolId = poolDepositEscrow.poolId;
+
+        require(
+            pools[poolId].owner != address(0x0),
+            "No Pool exists for this ID."
+        );
+
+        poolDepositEscrow.balance += msg.value;
+        poolDepositEscrows[escrowId] = poolDepositEscrow;
     }
 
     function compareStrings (string memory a, string memory b) public pure returns (bool) {
@@ -75,6 +150,14 @@ contract WorldPool is ReentrancyGuard {
         address owner; // pool owner
         string name; // pool name
         string description; // pool description
+        uint256 balance; // staked amount in wei
+        uint256 minStake; // minimum amount staked
+    }
+
+    struct PoolDepositEscrow {
+        bytes32 id; // escrow Id
+        address owner; // owner of deposited funds
+        bytes32 poolId; // pool Id
         uint256 balance; // staked amount in wei
     }
 }
