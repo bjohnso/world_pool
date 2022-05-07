a7d289822daf5d050c7f7770d5ccb75599315064
Brandon Johnson <brandon.johnson@Brandon-Johnson.local>
2022-05-07 13:44:00 +0200

    Escrow Contract

---
+++ contracts/Escrow.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract WorldPool is ReentrancyGuard {
+
+}
